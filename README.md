4024897269ee3fc0a6baebaeda2d102416981458
Bryan Nguyen <31323380+bdangnguyen@users.noreply.github.com>
2024-05-16 21:41:05 -0700

    Add utility class to generate AST Expr class files

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # README
 This is a Java 21 implementation of the Lox language in Java as described in Robert Nystrom's [Crafting Interpreters](https://craftinginterpreters.com/). The purpose of this was to learn more about interpreters and programming languages in general.
 
-## Installation
-* Install directly using Maven using ```mvn clean install```.
-* Alternatively run the included pom.xml with IDE of choice
+## Usage
+1. Install depedendencies and compile directly using Maven using ```mvn clean install```.
+2. Run ```java -cp .\target\lox-1.0.jar com.craftinginterpreters.lox.Lox``` to use the REPL shell.
+3. Run ```java -cp .\target\lox-1.0.jar com.craftinginterpreters.tool.GenerateAst .\src\main\java\com\craftinginterpreters\lox``` to generate the AST java class files. 
+
+Alternatively run the pom.xml using a IDE of choice.
 
 ## Testing
 Tests can be ran using ``` mvn clean test```
@@ -12,3 +15,19 @@ Tests can be ran using ``` mvn clean test```
 * [Maven](https://maven.apache.org/) is the build tool.
 * [JUnit5](https://junit.org/junit5/) is the testing framework.
 * [AssertJ](https://assertj.github.io/doc/) to provide more powerful assertions.
+
+## Grammar
+This current implementation of jlox has the following grammar.
+```
+expression     → literal
+               | unary
+               | binary
+               | grouping ;
+
+literal        → NUMBER | STRING | "true" | "false" | "nil" ;
+grouping       → "(" expression ")" ;
+unary          → ( "-" | "!" ) expression ;
+binary         → expression operator expression ;
+operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
+               | "+"  | "-"  | "*" | "/" ;
+```
---
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,77 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Utility class to generate the Expr class and its POJOs for representing the AST. Not tested
+ * explicitly due to the POJOs themselves being tested.
+ */
+public class GenerateAst {
+
+    public static void main(final String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+            "Binary   : final Expr left, final Token operator, final Expr right",
+            "Grouping : final Expr expression",
+            "Literal  : final Object value",
+            "Unary    : final Token operator, final Expr right"
+        ));
+    }
+
+    private static void defineAst(final String outputDir, final String baseName,
+     final List<String> types) throws IOException {
+        final String path = outputDir + "/" + baseName + ".java";
+        final PrintWriter writer = new PrintWriter(path, "UTF-8");
+    
+        writer.println("package com.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+    
+        for (final String type : types) {
+            final String className = type.split(":")[0].trim();
+            final String fields = type.split(":")[1].trim(); 
+            defineType(writer, baseName, className, fields);
+        }
+        
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(final PrintWriter writer, final String baseName,
+     final String className, final String fieldList) {
+        writer.println();
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        // Define fields in anonymous class.
+        final String[] fields = fieldList.split(", ");
+        for (final String field : fields) {
+            writer.println("        " + field + ";");
+        }
+
+        // Define constructor.
+        writer.println();
+        writer.println("        " + className + "(" + fieldList + ") {");
+
+        // Instantiate fields in constructor.
+        for (final String field : fields) {
+            final String name = field.split(" ")[2];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+
+        // Close Constructor
+        writer.println("        }");
+
+        // Define fields in abstract class.
+        writer.println("    }");
+    }
+}
